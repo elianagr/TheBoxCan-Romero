936df0e118a5cd0422f47d7b994018eebee8130d
Eliana Romero <eliana.romero@davinci.edu.ar>
2022-04-20 19:30:18 -0300

    ItemCount

---
+++ src/components/ItemCount.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react'
+
+const ItemCount = () => {
+
+    const [count, setCount] = useState(1)
+
+    useEffect( () => {
+    } , [])
+
+    useEffect( () => {
+    } , [count])
+
+    const addHandler = () => {
+        setCount(count + 1)
+    }
+
+    const removeHandler = () => {
+        if (count > 1) {
+            setCount(count - 1)
+        }
+    }
+
+
+  return (
+    <div>
+        <button onClick={removeHandler} class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-orange dark:text-orange focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white white:bg-gray-900 rounded-md group-hover:bg-opacity-75">-
+            </span>
+        </button>
+
+        <button type="button" class="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2"> {count} </button>
+
+        <button onClick={addHandler} class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-orange dark:text-orange focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white white:bg-gray-900 rounded-md group-hover:bg-opacity-75">+
+            </span>     
+        </button>
+    </div>
+
+    
+  )
+}
+
+export default ItemCount
--- src/components/ItemList.jsx
+++ src/components/ItemList.jsx
@@ -1,41 +1,56 @@
 import React from 'react'
 import correa from '../img/correa_azul.png';
+import ItemCount from './ItemCount';
 
 const ItemList = ({producto, precio}) => {
   return (
     <div>
         <div class="grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-        <a href="#" class="group">
-            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-            <img src={correa} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75" />
+            <div>
+                <a href="#" class="group">
+                    <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                    <img src={correa} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75" />
+                    </div>
+                    <h3 class="mt-4 text-sm text-gray-700">{producto}</h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900">${precio}</p>
+                </a>
+                <ItemCount/>
             </div>
-            <h3 class="mt-4 text-sm text-gray-700">{producto}</h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">${precio}</p>
-        </a>
 
-        <a href="#" class="group">
-            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-            <img src={correa} alt="Olive drab green insulated bottle with flared screw lid and flat top." class="w-full h-full object-center object-cover group-hover:opacity-75" />
+            <div>
+                <a href="#" class="group">
+                    <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                    <img src={correa} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75" />
+                    </div>
+                    <h3 class="mt-4 text-sm text-gray-700">{producto}</h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900">${precio}</p>
+                </a>
+                <ItemCount/>
             </div>
-            <h3 class="mt-4 text-sm text-gray-700">{producto}</h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">${precio}</p>
-        </a>
 
-        <a href="#" class="group">
-            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-            <img src={correa} alt="Person using a pen to cross a task off a productivity paper card." class="w-full h-full object-center object-cover group-hover:opacity-75" />
+            <div>
+                <a href="#" class="group">
+                    <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                    <img src={correa} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75" />
+                    </div>
+                    <h3 class="mt-4 text-sm text-gray-700">{producto}</h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900">${precio}</p>
+                </a>
+                <ItemCount/>
             </div>
-            <h3 class="mt-4 text-sm text-gray-700">{producto}</h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">${precio}</p>
-        </a>
 
-        <a href="#" class="group">
-            <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-            <img src={correa} alt="Hand holding black machined steel mechanical pencil with brass tip and top." class="w-full h-full object-center object-cover group-hover:opacity-75" />
+            <div>
+                <a href="#" class="group">
+                    <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
+                    <img src={correa} alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75" />
+                    </div>
+                    <h3 class="mt-4 text-sm text-gray-700">{producto}</h3>
+                    <p class="mt-1 text-lg font-medium text-gray-900">${precio}</p>
+                </a>
+                <ItemCount/>
             </div>
-            <h3 class="mt-4 text-sm text-gray-700">{producto}</h3>
-            <p class="mt-1 text-lg font-medium text-gray-900">${precio}</p>
-        </a>
+
+            
         </div>
     </div>
   )
